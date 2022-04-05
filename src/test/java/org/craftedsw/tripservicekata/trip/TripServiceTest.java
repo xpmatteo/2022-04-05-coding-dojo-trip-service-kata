c40b97da7e556fe571350bce8374fb52c3bed05c
Matteo Vaccari <mvaccari@thoughtworks.com>
2022-04-05 16:39:59 +0200

    Add assertj and initial failing test

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,13 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TripServiceTest {
-	
+    @Test
+    void name() {
+        assertThat(1).isEqualTo(0);
+    }
 }
